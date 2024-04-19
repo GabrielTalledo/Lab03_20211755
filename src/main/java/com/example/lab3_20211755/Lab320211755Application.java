6f77e6a19e7fa9b65b8a9bdabae22ab3ced7150b
Gabriel <gabriel_alejandro.tm@hotmail.com>
2024-04-18 20:34:31 -0500

    feat: Comienzo del laboratorio

---
+++ src/main/java/com/example/lab3_20211755/Lab320211755Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20211755;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320211755Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab320211755Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab3_20211755/Lab320211755ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20211755;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab320211755ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
